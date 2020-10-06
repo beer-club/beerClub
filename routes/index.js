a9d69e37dc8f9ffc566e806499705375b5f340ca
fvendrasco <felipe@vendrasco.com.br>
2020-10-05 22:02:07 -0300

    add cervejas e kits

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
 
+router.get('/cervejas', function(req, res, next) {
+  res.render('cervejas', { title: 'Express' });
+});
+
+router.get('/kits', function(req, res, next) {
+  res.render('kits', { title: 'Express' });
+});
+
 module.exports = router;
