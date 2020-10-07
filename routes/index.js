e6e1aacd61a1d60204ec71781ade0b02386a4233
fvendrasco <felipe@vendrasco.com.br>
2020-10-07 20:25:34 -0300

    Header, Rotas

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Express' });
+});
+
+router.get('/clube', function(req, res, next) {
+  res.render('clube', { title: 'Express' });
 });
 
 router.get('/cervejas', function(req, res, next) {
