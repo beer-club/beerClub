5b3915355d9a5b32ee64870123569a00eef20934
Thiago Cerqueira <thiague@air-de-thiago.home>
2020-10-13 23:12:16 -0300

    SQL para add Cervejas

---
+++ public/js/comandosSql.js
@@ -0,0 +1,33 @@
+/*Adicionar Tipo de produto*/
+
+/*
+USE beerclub;
+INSERT INTO productTypes (typeCode, typeName, createdAt, updatedAt) VALUES (1, 'Cerveja', NOW(), NOW());
+INSERT INTO productTypes (typeCode, typeName, createdAt, updatedAt) VALUES (2, 'Assinatura', NOW(), NOW());
+INSERT INTO productTypes (typeCode, typeName, createdAt, updatedAt) VALUES (3, 'Kits', NOW(), NOW());
+INSERT INTO productTypes (typeCode, typeName, createdAt, updatedAt) VALUES (4, 'Acessorios', NOW(), NOW());
+SELECT * from productTypes;
+*/
+
+/*Adicionar Tipo de cerveja*/
+/*
+USE beerclub;
+INSERT INTO kindBeers (kindCode, kindNAme, createdAt, updatedAt) VALUES (1, 'IPA', NOW(), NOW());
+INSERT INTO kindBeers (kindCode, kindNAme, createdAt, updatedAt) VALUES (2, 'Trigo', NOW(), NOW());
+INSERT INTO kindBeers (kindCode, kindNAme, createdAt, updatedAt) VALUES (3, 'Lager', NOW(), NOW());
+SELECT * from kindBeers;
+*/
+
+/*Adicionar Produtos*/
+/*
+USE beerclub;
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (1, 'Cerveja Antuérpia American IPA 500ml', 19.50, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175135-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (2, 'Cerveja Brooklyn Eats IPA 350ml', 15.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/174343-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (3, 'Cerveja Madalena IPA 355ml', 13.52, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/174792-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (4, 'Cerveja Goose Island Midway 355ml', 9.99, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/172313-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (5, 'Cerveja Brewdog Clockwork Tangerine 330ml', 25.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/172313-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (6, 'Cerveja Dom Casmurro IPA 600ml', 19.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175813-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
+SELECT * from products;
+*/
+
+
