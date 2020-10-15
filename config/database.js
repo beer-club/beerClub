e030aff322c27f8ae34e7a773d9c2b5f37526cae
fvendrasco <felipe@vendrasco.com.br>
2020-10-14 21:58:38 -0300

    css+menu

--- config/database.js
+++ config/database.js
@@ -1,10 +1,6 @@
 module.exports = {
     username: 'root',
-<<<<<<< HEAD
     password: '*V3ndr@sc0*',
-=======
-    password: 'root1234',
->>>>>>> db5ebd115d41dc9a06bf0665345a1956e57834fc
     database: 'beerclub',
     host: '127.0.0.1',
     dialect: 'mysql',
