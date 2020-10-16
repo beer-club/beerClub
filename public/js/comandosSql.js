2179e67ecd04bf3dfafee949b7b7e2a80e2cb9a2
Thiago Cerqueira <thiague@air-de-thiago.home>
2020-10-16 16:50:06 -0300

    Query para adicao de kits e acessorios

--- public/js/comandosSql.js
+++ public/js/comandosSql.js
@@ -18,7 +18,7 @@ INSERT INTO kindBeers (kindCode, kindNAme, createdAt, updatedAt) VALUES (3, 'Lag
 SELECT * from kindBeers;
 */
 
-/*Adicionar Produtos*/
+/*Adicionar Produtos Cervejas*/
 /*
 USE beerclub;
 INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (1, 'Cerveja Antuérpia American IPA 500ml', 19.50, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175135-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
@@ -27,6 +27,30 @@ INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCod
 INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (4, 'Cerveja Goose Island Midway 355ml', 9.99, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/172313-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
 INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (5, 'Cerveja Brewdog Clockwork Tangerine 330ml', 25.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/176649-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
 INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (6, 'Cerveja Dom Casmurro IPA 600ml', 19.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175813-176-176?width=176&height=176&aspect=true', 1, 1, NOW(), NOW());
+SELECT * from products;
+*/
+
+/*Adicionar Produtos Kits*/
+/*
+USE beerclub;
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (7, 'Kit Colorado', 39.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/174824-176-176?width=176&height=176&aspect=true', 1, 3, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (8, 'Kit Top sellers', 63.85, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/176792-176-176?width=176&height=176&aspect=true', 1, 3, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (9, 'Kit Corona Blaster', 329.78, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/177049-176-176?width=176&height=176&aspect=true', 1, 3, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (10, 'Kit Patagonia', 49.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175796-176-176?width=176&height=176&aspect=true', 1, 3, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (11, 'Kit Goose Island', 69.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175380-176-176?width=176&height=176&aspect=true', 1, 3, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (12, 'Kit Madalena', 47.92, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/177490-176-176?width=176&height=176&aspect=true', 1, 3, NOW(), NOW());
+SELECT * from products;
+*/
+
+/*Adicionar Produtos Acessorios*/
+/*
+USE beerclub;
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (13, 'Copo Budweiser FIFA', 2.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/170057-176-176?width=176&height=176&aspect=true', 1, 4, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (14, 'Camiseta Corona', 59.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/177964-176-176?width=176&height=176&aspect=true', 1, 4, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (15, 'Frigobar Colorado 37L', 1590, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/173882-176-176?width=176&height=176&aspect=true', 1, 4, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (16, 'Abridor Budweiser', 14.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/169941-176-176?width=176&height=176&aspect=true', 1, 4, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (17, 'Balde Budweiser', 89.90, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175292-176-176?width=176&height=176&aspect=true', 1, 4, NOW(), NOW());
+INSERT INTO products (productCode, productName, price, imagem, kindBeers_kindCode, productTypes_typeCode, createdAt, updatedAt) VALUES (18, 'Balde Bohemia Aura Lager', 31.96, 'https://emporiodacerveja.vtexassets.com/arquivos/ids/175292-176-176?width=176&height=176&aspect=true', 1, 4, NOW(), NOW());
 SELECT * from products;
 */
 
